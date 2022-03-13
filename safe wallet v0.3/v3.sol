2e8c80c887c068a60f96f8e6ffc7509fd076485b
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-03-13 13:08:15 +0330

    Create v3.sol

---
+++ safe wallet v0.3/v3.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
+
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// need: checker, conditions, require reverts
+// check exist id, approved id,etc...
+
+contract NftWallet is Ownable{
+    mapping(uint256 => address) public collector; // sender=>covrage
+    uint256 covrage; // saved item id (a db), seprate owners
+    uint256 private holdTime = 1 weeks;
+    uint256 private _now;
+
+    constructor(){
+        _init();
+    }
+
+    function _init() internal {
+        _now = block.timestamp;
+        holdTime += _now;
+    }
+
+    function receiveNft(ERC721 _nft, uint id) external{
+        require(_nft.ownerOf(id) == msg.sender, "be serius!");
+        // _nft.setApprovalForAll(address(this), true);
+        _nft.approve(address(this), id);
+        _nft.transferFrom(msg.sender, address(this), id);
+        collector[covrage] = msg.sender;
+        covrage += 1;
+    }
+    
+    function sendNft(ERC721 nft, address spender, uint id) external onlyOwner{
+        // require(check owner)
+        require(block.timestamp >= holdTime, "wait for it");
+        require(spender != address(0), "i hate to burn nft`s");
+        nft.transferFrom(address(this), spender, id);
+        collector[covrage] = address(0); 
+    }
+}
+
+// after mint, approve NftWallet smartcontract address
+contract NftTest is ERC721 {
+    uint id = 1;
+    constructor() ERC721("test","TST"){
+        _mint(msg.sender, id);
+        id += 1;
+    }
+    
+    function mint() external{
+        _mint(msg.sender, id);
+        id += 1;
+    }
+}
